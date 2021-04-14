1fddbc01880288b2d53ab438849dad00b5b5f566
Santa Chakraborty <chakraborty.santa@gmail.com>
2021-04-14 08:03:19 -0400

    commit

---
+++ Week2_HW2_Functions/HW-1-2-3-4/hw1234.js
@@ -0,0 +1,51 @@
+
+// ...........................HOMEWORK 1....................................
+
+function maxOfTwoNumbers(num1,num2){
+   if(num1>num2){
+     return num1;
+   }else{
+     return num2;
+   }
+}
+
+
+// .............................HOMEWORK 2............................
+
+// let num1,num2,num3;
+// function maxOfThreeNumbers(num1,num2,num3){
+//    if((num1>num2) && (num1>num3)){
+//         return num1;
+//    }else if(num2>num3 && num2>num1){
+//      return num2;
+//    }else{
+//      return num3;
+//    }
+// }
+// ...................................HOMEWORK 3....................................
+
+// function isCharacterAVowel(ch){
+//   if(ch=='a' || ch=='e' || ch=='i' ||ch=='o'|| ch=='u'){
+//      return " a vowel";
+//   }else{
+//     return "not a vowel";
+//   }
+// }
+// ......................................HOMEWORK 4.........................
+
+
+// function sumArray(array){
+//    let sum=0;
+//    for(let i=0; i<array.length; i++){
+//      sum+=array[i];
+//     }
+//    return sum;
+//   }
+  
+//   function multiplyArray(array){
+//     let product=1;
+//     for(let i=0; i<array.length; i++){
+//       product*=array[i];
+//      }
+//     return product;
+//    }
